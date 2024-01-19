e4cd5d635cd4f4ca18da3be194dc88a478feb0e9
Darshan S <darshan.s@marmeto.com>
2024-01-19 10:18:29 +0530

    pre UI update

--- assets/take-pledge.js
+++ assets/take-pledge.js
@@ -4,6 +4,7 @@ const openPledgeFormButton = document.querySelector('.pledge__start-button butto
 const closePledgeFormButton = document.querySelector('.pledge__submit-button');
 const pledgeFormContainer = document.getElementById('pledge__form-container');
 const pledgeForm = document.getElementById('pledge-Form');
+let responseData;
 
 nextButton.addEventListener('click', ()=> {
   const pledgeInitiateContainer = document.querySelector('.pledge__bottom-container');
@@ -16,35 +17,36 @@ openPledgeFormButton.addEventListener('click', () => {
 
 closePledgeFormButton.addEventListener('click', closePledgeForm);
 
-pledgeForm.addEventListener('submit', (e)=> {
-  e.preventDefault();
+pledgeForm.addEventListener('submit', handlePledgeSubmit)
 
-  // show thank you message
-  const thankYouContainer = document.querySelector('.thank-you__message-container');
-  thankYouContainer.classList.remove('hide');
+function handlePledgeSubmit() {
+  event.preventDefault();
 
   const data = new FormData(pledgeForm);
-  const action = e.target.action;
-
-  console.log('Form data:');
-  for (let [key, value] of data.entries()) {
-    console.log(`${key}: ${value}`);
-  }
-  console.log('action:', action);
+  const action = event.target.action;
 
   fetch(action, {
     method: 'POST',
     body: data,
   })
     .then((response) => response.json())
-    .then((response) => {
-      console.log(response);
-      const pledgeCountFromSheet = response.row - 1;
-      const pledgeCointUpdate = document.querySelector('.thank-you__message-container .pledge-count__update');
-      pledgeCointUpdate.innerHTML = pledgeCountFromSheet;
+    .then((response) => responseData = response)
+    .finally(()=> {
+      debugger;
+      const pledgeCountFromSheet = responseData.row - 1;
+      const pledgeCountUpdate = document.querySelector('.thank-you__message-container .pledge-count__update');
+      pledgeCountUpdate.innerHTML = pledgeCountFromSheet;
+      
+      const pledgeMainContainer = document.querySelector('.pledge__main-container');
+      pledgeMainContainer.classList.add('hide');
+
+      // show thank you message
+      const thankYouContainer = document.querySelector('.thank-you__message-container');
+      thankYouContainer.classList.remove('hide');
       closePledgeForm();
-    })
-});
+      pledgeForm.removeEventListener(handlePledgeSubmit);
+    });
+};
 
 function closePledgeForm() {
   pledgeFormContainer.close();
