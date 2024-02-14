0f8d64302c9e9bed734f494f047aea2d5ee5f94a
Balaji A <93375333+Balaji21121109@users.noreply.github.com>
2024-02-14 17:54:26 +0530

    Account page Changes

--- assets/account-page-tabs.js
+++ assets/account-page-tabs.js
@@ -1,12 +1,12 @@
-function open(evt, tabId) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tab-content");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+function openTab(evt, tabId) {
+  var i, tabContents, tabLinks;
+  tabContents = document.querySelectorAll(".tab-content");
+  for (i = 0; i < tabContents.length; i++) {
+    tabContents[i].style.display = "none";
   }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].classList.remove("is--active");
+  tabLinks = document.querySelectorAll(".tablinks");
+  for (i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].classList.remove("is--active");
   }
   var tabElement = document.getElementById(tabId);
   if (tabElement.style.display !== "block") {
@@ -17,23 +17,28 @@ function open(evt, tabId) {
     evt.currentTarget.classList.remove("is--active");
   }
 }
-var activeElement = document.querySelector(".is--active");
-if (activeElement) {
-  activeElement.click();
-}
-var tablinks = document.getElementsByClassName('tablinks');
-for (var i = 0; i < tablinks.length; i++) {
-  tablinks[i].addEventListener("click", function(event) {
-    var tabId = event.currentTarget.getAttribute("data-tab");
-    open(event, tabId);
-  });
-}
 
-document.querySelector('.tablinks.details.whishlist').addEventListener('click', function() {
-  // Redirect to the desired page
-  window.location.href = 'apps/advanced-wishlist';
-});
-document.querySelector('.account_wishlist.tablinks').addEventListener('click', function() {
-  // Redirect to the desired page
-  window.location.href = 'apps/advanced-wishlist';
+document.addEventListener("DOMContentLoaded", function() {
+  var activeElement = document.querySelector(".is--active");
+  if (activeElement) {
+    activeElement.click();
+  }
+  
+  var tabLinks = document.querySelectorAll('.tablinks');
+  for (var i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].addEventListener("click", function(event) {
+      var tabId = event.currentTarget.getAttribute("data-tab");
+      openTab(event, tabId);
+    });
+  }
+  
+  document.querySelector('.tablinks.details.whishlist').addEventListener('click', function() {
+    // Redirect to the desired page
+    window.location.href = 'apps/advanced-wishlist';
+  });
+  
+  document.querySelector('.account_wishlist.tablinks').addEventListener('click', function() {
+    // Redirect to the desired page
+    window.location.href = 'apps/advanced-wishlist';
+  });
 });
