995245d2738af24a06bbc0c0691c0c51694229e4
Balaji A <93375333+Balaji21121109@users.noreply.github.com>
2024-02-14 19:14:14 +0530

    Feedbacks

--- assets/account-page-tabs.js
+++ assets/account-page-tabs.js
@@ -1,14 +1,13 @@
 function openTab(evt, tabId) {
-  var i, tabContents, tabLinks;
-  tabContents = document.querySelectorAll(".tab-content");
-  for (i = 0; i < tabContents.length; i++) {
-    tabContents[i].style.display = "none";
-  }
-  tabLinks = document.querySelectorAll(".tablinks");
-  for (i = 0; i < tabLinks.length; i++) {
-    tabLinks[i].classList.remove("is--active");
-  }
+  document.querySelectorAll(".tab-content").forEach(function (tabContent) {
+    tabContent.style.display = "none";
+  });
+  document.querySelectorAll(".tablinks").forEach(function (tabLink) {
+    tabLink.classList.remove("is--active");
+  });
+
   var tabElement = document.getElementById(tabId);
+  // Toggle display of selected tab content
   if (tabElement.style.display !== "block") {
     tabElement.style.display = "block";
     evt.currentTarget.classList.add("is--active");
@@ -18,27 +17,32 @@ function openTab(evt, tabId) {
   }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   var activeElement = document.querySelector(".is--active");
   if (activeElement) {
     activeElement.click();
   }
-  
-  var tabLinks = document.querySelectorAll('.tablinks');
-  for (var i = 0; i < tabLinks.length; i++) {
-    tabLinks[i].addEventListener("click", function(event) {
+
+  var tabLinks = document.querySelectorAll(".tablinks");
+
+  Array.from(tabLinks).forEach(function (tabLink) {
+    tabLink.addEventListener("click", function (event) {
       var tabId = event.currentTarget.getAttribute("data-tab");
       openTab(event, tabId);
     });
-  }
-  
-  document.querySelector('.tablinks.details.whishlist').addEventListener('click', function() {
-    // Redirect to the desired page
-    window.location.href = 'apps/advanced-wishlist';
   });
-  
-  document.querySelector('.account_wishlist.tablinks').addEventListener('click', function() {
-    // Redirect to the desired page
-    window.location.href = 'apps/advanced-wishlist';
-  });
+
+  document
+    .querySelector(".tablinks.details.whishlist")
+    .addEventListener("click", function () {
+      // Redirect to the desired page
+      window.location.href = "apps/advanced-wishlist";
+    });
+
+  document
+    .querySelector(".account_wishlist.tablinks")
+    .addEventListener("click", function () {
+      // Redirect to the desired page
+      window.location.href = "apps/advanced-wishlist";
+    });
 });
