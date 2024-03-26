dceaa8f76d96436eb9b7e2d40caae2ba9955e2ca
Balaji A <93375333+Balaji21121109@users.noreply.github.com>
2024-03-26 12:34:19 +0530

    Changes

--- assets/theme.js
+++ assets/theme.js
@@ -9889,3 +9889,45 @@ if (console && console.log) {
     document.dispatchEvent(new CustomEvent("page:loaded"));
   });
 })();
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Get the modal
+var modal = document.getElementById("customizer-Modal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("customizer-button");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("customizer_close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+  document.querySelector("body").style.height = "100%";
+  document.querySelector("body").style.overflow = "hidden";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+  document.querySelector("body").style.overflow = "scroll";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    document.querySelector("body").style.overflow = "scroll";
+  }
+}
